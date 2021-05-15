68c809eeb0ad8b62a9fbc2f737cb001b5177cf03
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-05-15 22:36:27 +0200

    Add user environ Python packaging

---
+++ nixpkgs/overlays/myEnv.nix
@@ -0,0 +1,23 @@
+# ~/.config/nixpkgs/overlays/myEnv.nix
+self: super: {
+  myEnv = super.buildEnv {
+    name = "myEnv";
+    paths = [
+      # A python 3 interpreter with some packages
+      (self.python3.withPackages (
+        ps: with ps; [
+          pyflakes
+          pytest
+          python-language-server
+        ]
+      ))
+
+
+      # Some other packages we'd like as part of this env
+      self.mypy
+      self.black
+      self.ripgrep
+      self.tmux
+    ];
+  };
+}
