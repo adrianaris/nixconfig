65b358457c29f011b9df2328671ea1d7965cfc0e
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-05-25 14:50:02 +0200

    Update configs and change folder structure to resemble system

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,161 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./vim.nix
+      ./vscode.nix
+    ];
+ 
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp9s0f0.useDHCP = true;
+  networking.interfaces.wlp8s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+  
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.adi = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    home = "/home/adi";
+    description = "Adrian Serbanescu";
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget vim_configurable
+    tmux
+    firefox
+    google-chrome
+    python3
+    gcc
+    gtk3
+    nodejs
+    neovim
+    emacs
+    vscode
+    zsh
+    oh-my-zsh
+    nix-zsh-completions
+    git
+    sqlite
+    docker
+    libreoffice
+    okular
+    qimgv
+    obs-studio
+    neofetch
+    ranger
+    irssi
+    weechat
+    vlc
+    scrot
+    discord
+
+    # systemwide python packages
+#    (python38.withPackages(ps: with ps; [ numpy toolz]))
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # zsh default
+  programs.zsh.enable = true;
+  programs.zsh.ohMyZsh.customPkgs = [
+    pkgs.nix-zsh-completions
+    # and even more...
+  ];
+  # oh-my-zsh config
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" "python" "man" ];
+    theme = "agnoster";
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+  # networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  # AUTO Upgrades
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
+  nixpkgs.config.allowUnfree = true; 
+
+}
---
+++ etc/nixos/vim.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+let
+  jsxpretty= pkgs.vimUtils.buildVimPlugin {
+    name = "vim-jsx-pretty";
+    src = pkgs.fetchFromGitHub {
+      owner = "MaxMEllon";
+      repo = "vim-jsx-pretty";
+      rev = "6989f1663cc03d7da72b5ef1c03f87e6ddb70b41";
+      sha256 = "16d76jvfb1cq591i06fnmgzv0q16g89zz7cmvgvj24vap2wgkbp8";
+    };
+  };
+in
+{
+  environment.variables = { EDITOR = "vim"; };
+
+  environment.systemPackages = with pkgs; [
+    (vim_configurable.customize {
+      name = "mvi";
+      vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        start = [ 
+          vim-nix 
+          vim-lastplace 
+          jsxpretty
+          polyglot
+        ];
+        opt = [];
+      };
+      vimrcConfig.customRC = ''
+	set number	
+	set linebreak	
+	set showbreak=+++ 	
+	set textwidth=100
+	set showmatch	
+	set visualbell
+	 
+	set hlsearch
+	set smartcase
+	set ignorecase
+	set incsearch
+	 
+	set autoindent
+	set shiftwidth=4
+	set smartindent
+	set smarttab
+	set softtabstop=4
+	set ruler	
+	 
+	set undolevels=1000
+	set backspace=indent,eol,start
+      '';
+    }
+  )
+   (neovim.override {
+      vimAlias = true;
+      configure = {
+        packages.myPlugins = with pkgs.vimPlugins; {
+          start = [ vim-lastplace vim-nix ]; 
+          opt = [];
+        };
+        customRC = ''
+          set number 
+        '';
+      };
+    }
+  )
+];
+}
+
