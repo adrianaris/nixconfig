d79ff1daa90ef26bf484e825af8fe66f2d98e6c7
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-01-16 18:19:36 +0100

    called updateNixosConfig script

--- nixos/scripts/updateNixosConfig.nix
+++ nixos/scripts/updateNixosConfig.nix
@@ -5,4 +5,8 @@ writeShellScriptBin "updateNixosConfig.sh" ''
   rm -rf ~/nixconfig/nixos/
   cp -r /etc/nixos/ ~/nixconfig
   rm ~/nixconfig/nixos/hardware-configuration.nix
+  cd ~/nixconfig
+  git add .
+  git commit -m 'called updateNixosConfig script'
+  git push origin main
 ''
