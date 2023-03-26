a09b9a28a375815e075d254d0a066697d77e90d8
Adrian Serbanescu <adrian@adrianserbanescu.com>
2023-03-26 21:40:33 +0200

    called updateNixosConfig script

--- nixos/hosts/nixos/scripts/updateNixosConfig.nix
+++ nixos/hosts/nixos/scripts/updateNixosConfig.nix
@@ -4,6 +4,7 @@ writeShellScriptBin "updateNixosConfig.sh" ''
   #!/bin/sh
   rm -rf ~/nixconfig/nixos/
   cp -r /etc/nixos/ ~/nixconfig
+  cp -r ~/dotfiles ~/nixconfig
   cd ~/nixconfig
   git add .
   git commit -m 'called updateNixosConfig script'
